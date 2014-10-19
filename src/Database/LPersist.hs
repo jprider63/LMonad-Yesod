9227e36f82a62b975d7d9510dcdb278fa8642b78
James Parker <dev@jamesparker.me>
2014-10-19 17:39:10 -0400

    compilation fixes

--- src/Database/LPersist.hs
+++ src/Database/LPersist.hs
@@ -1,5 +1,8 @@
 module Database.LPersist where
 
+import Database.Persist
+import LMonad
+
 -- | `LEntity` typeclass to taint labels when reading, writing, and creating entity fields.
 -- Internally used to raise the current label on database calls. 
 -- `mkLabels` automatically generates instances of `LEntity` for your model. 
