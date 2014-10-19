1860ec0e143b46f475da7e638914380879bf1058
James Parker <dev@jamesparker.me>
2014-10-19 17:33:55 -0400

    added LEntity typeclass

---
+++ src/Database/LPersist.hs
@@ -0,0 +1,9 @@
+module Database.LPersist where
+
+-- | `LEntity` typeclass to taint labels when reading, writing, and creating entity fields.
+-- Internally used to raise the current label on database calls. 
+-- `mkLabels` automatically generates instances of `LEntity` for your model. 
+class Label l => LEntity l e where
+    raiseLabelRead :: LMonad m => Entity e -> LMonadT l m ()
+    raiseLabelWrite :: LMonad m => Entity e -> LMonadT l m ()
+    raiseLabelCreate :: LMonad m => e -> LMonadT l m ()
