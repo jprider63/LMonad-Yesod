9f8fda0e2e0ed49a9e1598a5465a0cbf73c70fff
James Parker <dev@jamesparker.me>
2018-05-22 01:15:48 -0400

    forgot file

---
+++ src/Database/LPersist/TCB.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Database.LPersist.TCB where
+
+import Database.Persist (Entity(..), Key)
+import LMonad.TCB
+
+-- | ADT wrapper for protected entities. Analagous to Entity.
+data PEntity l e = (ProtectedEntity l e) => PEntity (Key e) (Protected e)
+
+-- | Typeclass for protected entities.
+-- `mkLabels` automatically generates these instances.
+type family Protected e
+class Label l => ProtectedEntity l e | e -> l where
+    -- toProtected :: LMonad m => Entity e -> LMonadT l m (Protected e)
+    -- TCB since we don't do any checks when labelling.
+    toProtectedTCB :: Entity e -> Protected e
+
+toProtectedWithKeyTCB :: (ProtectedEntity l e) => Entity e -> PEntity l e
+toProtectedWithKeyTCB r = 
+    let p = toProtectedTCB r in
+    PEntity (entityKey r) p
+
