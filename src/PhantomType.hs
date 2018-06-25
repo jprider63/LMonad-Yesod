608ea8cde156c3c3a67e1789c7bc1469361a7ec6
James Parker <dev@jamesparker.me>
2018-06-25 17:51:25 -0400

    actually create the phantom type

--- src/PhantomType.hs
+++ src/PhantomType.hs
@@ -16,9 +16,14 @@ phantomType s = case words s of
     conT:rest ->
         if length conT <= 1 || Char.isLower (head conT) then
             error $ "Invalid type constructor `" ++ conT ++ "`"
-        else
-            let con = ConT $ mkName conT in
-            lift $ List.foldl' (\acc typ -> AppT acc (ConT (mkName typ))) con rest
+        else do
+            let con = ConT $ mkName conT
+            let typ = List.foldl' (\acc typ -> AppT acc (ConT (mkName typ))) con rest
+            typE <- lift typ
+            let pE = AppE (ConE 'PhantomType) typE
+            let pT = AppT (ConT ''PhantomType) typ
+            return $ SigE pE pT
+
 
 -- instance Lift Type where
 --     lift = $(makeLift ''Type)
